710a976ae2e90c5035b5eaec81f8e44952d4d302
ciupedavid <63590129+ciupedavid@users.noreply.github.com>
2022-10-27 18:02:02 +0300

    Delete step2.py

--- step2.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-
-# Unzip files
-
-with zipfile.ZipFile(r'C:\Users\davciupe\Downloads\LTspice September 14, 2022.zip') as zip_ref:
-     zip_ref.extractall(r'C:\Users\davciupe\Downloads')
-
-with zipfile.ZipFile(r'C:\Users\davciupe\Downloads\Raw Data Export - September 14, 2022.zip') as zip_ref:
-    zip_ref.extractall(r'C:\Users\davciupe\Downloads')
